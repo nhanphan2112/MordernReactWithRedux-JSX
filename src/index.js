f58ab55338209c011d499059bed5e786640bfb6a
Nhan Phan <nphanacc@gmail.com>
2021-05-25 11:08:15 -0700

    13-Displaying Content With Functional Component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a react component
+//below is a functional component
+const App = () => {
+	return <div>Hi there!</div>;
+};
 
 // Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+//Below code added to alow Live Reloading
+if (module.hot) {
+	module.hot.accept();
+}
