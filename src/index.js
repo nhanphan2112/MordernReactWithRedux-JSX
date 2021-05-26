b928e631df329ecdc556caf82f82262b51b986a2
Nhan Phan <nphanacc@gmail.com>
2021-05-26 10:09:30 -0700

    22-Values JSX Cannot Show

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function getButtonText() {
-	return "Click on me!";
-}
 // Create a react component
 //below is a functional component
 const App = () => {
+	const buttonText = { text: "Click me" };
+  const labelText = 'Enter name:';
+
 	return (
 		<div>
 			<label className="label" for="name">
-				Enter name:
+				{labelText}
 			</label>
 			<input id="name" type="text" />
 			{/* <button style="background-color: blue; color:white;">Submit</button> */}
 			{/* The usesage of single quote '' in line 16 is for non JSX properties such as css html styling  */}
-			<button style={{ backgroundColor: "blue", color: "white" }}>{getButtonText()}</button>
+			<button style={{ backgroundColor: "blue", color: "white" }}>{buttonText.text}</button>
 		</div>
 	);
 };
