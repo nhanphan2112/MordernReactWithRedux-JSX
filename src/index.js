85664c6f094473e285273cbc381068e79db2ceef
Nhan Phan <nphanacc@gmail.com>
2021-05-26 09:52:29 -0700

    21-Referencing JS Variables in JSX

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+	return "Click on me!";
+}
 // Create a react component
 //below is a functional component
 const App = () => {
 	return (
 		<div>
-			<label class="label" for="name">
+			<label className="label" for="name">
 				Enter name:
 			</label>
 			<input id="name" type="text" />
 			{/* <button style="background-color: blue; color:white;">Submit</button> */}
-      {/* The usesage of single quote '' in line 16 is for non JSX properties such as css html styling  */}
-			<button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+			{/* The usesage of single quote '' in line 16 is for non JSX properties such as css html styling  */}
+			<button style={{ backgroundColor: "blue", color: "white" }}>{getButtonText()}</button>
 		</div>
 	);
 };
